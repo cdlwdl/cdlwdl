d0b5564beeab07ee7e7d0178fd4a12ebece70440
cdlwdl <312074542@qq.com>
2019-06-24 10:18:59 +0800

    2019-06.22-01

--- Python_First.py
+++ Python_First.py
@@ -1,7 +1,10 @@
 
 #计算1+2的结果
-str1 = '计算%d+%d的结果'%(1,2)
-str2 = '计算结果为:%d'%(1+2)
+num1 = 1
+num2 = 2
+result = num1 + num2
+desc = '计算%d+%d的结果'%(num1,num2)
+result = '计算结果为:%d'%(result)
 
 #打印输出结果
-print(str1 + '\n' + str2)
+print(desc + '\n' + result)
